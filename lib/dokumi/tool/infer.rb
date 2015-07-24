6c678ffd7589c2bc6c2ae5bafb2214cca9afe2d9
Kentaro Takiguchi <kentaro-takiguchi@cookpad.com>
2015-07-24 17:26:35 +0900

    [nits] Make code simpler

--- lib/dokumi/tool/infer.rb
+++ lib/dokumi/tool/infer.rb
@@ -7,15 +7,13 @@ module Dokumi
         def parse_report(target_project)
           report_path = Support.make_pathname(INFER_REPORT_FILE)
 
-          File.open(report_path) do |file|
-            report = JSON.load(file)
-            report.map do |info|
-              {
-                description: info["qualifier"],
-                file_path: info["file"],
-                line: info["line"].to_i,
-              }
-            end
+          report = JSON.parse(File.read(report_path))
+          report.map do |info|
+            {
+              description: info["qualifier"],
+              file_path: info["file"],
+              line: info["line"].to_i,
+            }
           end
         end
       end
