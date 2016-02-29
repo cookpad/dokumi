49fff91e44cf0198c6828d5daf4ece1e674e7f9e
Ryu Kato <ryu-kato@cookpad.com>
2016-02-29 14:17:27 +0900

    fix code styles

--- lib/dokumi/tool/android/findbugs.rb
+++ lib/dokumi/tool/android/findbugs.rb
@@ -3,6 +3,7 @@ module Dokumi
     class Android
       class FindBugs
         FINDBUGS_REPORT_FILE = "build/reports/findbugs/findbugs.xml"
+        RANK_ERROR_THRESHOLD = 4
 
         class << self
           def parse_report(target_project)
@@ -11,7 +12,7 @@ module Dokumi
               report = Nokogiri::XML(file)
 
               report.xpath("//BugInstance").map do |info|
-                rank = info.attribute('rank').value
+                rank = info.attribute("rank").value
                 source_path = info.xpath("SourceLine/@sourcepath").first.to_s
                 file_path = Support.make_pathname(target_project).join("src/main/java", source_path)
 
@@ -19,7 +20,7 @@ module Dokumi
                     description: info.xpath("LongMessage/text()").first.to_s,
                     file_path: file_path,
                     line: info.xpath("SourceLine/@start").first.to_s.to_i,
-                    type: rank.to_i > 4 ? :warning : :error,
+                    type: rank.to_i > RANK_ERROR_THRESHOLD ? :warning : :error,
                 }
               end
             end
